ddf4d85539d1882cbdea6cccf334b4c74f7da2c8
Ben Argo <ben@benargo.com>
2025-08-23 15:22:11 +0100

    Add discord redirect (temporary)

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,18 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+/**
+ * Comps spreadsheet redirect
+ */
 Route::get('/comps', function () {
     return redirect('https://docs.google.com/spreadsheets/d/1SYaMOFDtXxdRm7gQz6nG7c_B-N7rsf7P7QIRipkJkwg/view?usp=sharing', 303);
+});
+
+/**
+ * Discord redirect
+ */
+Route::get('/discord', function () {
+    return redirect('https://discord.gg/pM6haPnQRt', 303);
 });
 
 require __DIR__.'/auth.php';
