e4bb306b65bb310785e34fdc0c01c59239c01356
Ben Argo <benargo@users.noreply.github.com>
2026-02-09 12:24:31 +0000

    Disable the scheduled job

--- routes/console.php
+++ routes/console.php
@@ -3,5 +3,5 @@
 use App\Jobs\CheckLevelCapAchievements;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::job(CheckLevelCapAchievements::class)->everyFiveMinutes();
+// Schedule::job(CheckLevelCapAchievements::class)->everyFiveMinutes();
 Schedule::call('model:prune')->dailyAt('03:00');
