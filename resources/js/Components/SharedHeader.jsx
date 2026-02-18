708e342e63c627e4a2368692f314864a8b886f68
Ben Argo <benargo@users.noreply.github.com>
2026-02-18 00:02:47 +0000

    Add new Karazhan background

--- resources/js/Components/SharedHeader.jsx
+++ resources/js/Components/SharedHeader.jsx
@@ -1,6 +1,6 @@
 export default function SharedHeader({ title, backgroundClass = 'bg-masthead' }) {
     return (
-        <div className={`${backgroundClass} py-24 text-white`}>
+        <div className={`${backgroundClass} py-32 text-white`}>
             <div className="container mx-auto px-4">
                 <h1 className="text-4xl font-bold text-center">
                     {title}
