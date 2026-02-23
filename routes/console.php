b84edec19c690e82a4b15fd532b12d656a63b805
Ben Argo <benargo@users.noreply.github.com>
2026-02-23 20:47:21 +0000

    Add scheduled task to refresh Warcraft Logs reports

--- routes/console.php
+++ routes/console.php
@@ -6,12 +6,17 @@ use Illuminate\Support\Facades\Schedule;
 /**
  * Synchronise Discord roles every hour to ensure we have the latest roles and permissions.
  */
-Schedule::job(SyncDiscordRoles::class)->hourly()->name('sync-discord-roles');
+Schedule::command('app:sync-discord')->hourly()->name('sync-discord')->withoutOverlapping();
+
+/**
+ * Refresh Warcraft Logs reports every 6 hours to keep the data up to date.
+ */
+Schedule::command('app:refresh-warcraft-logs-reports --latest')->everySixHours()->name('refresh-warcraft-logs-reports')->withoutOverlapping();
 
 /**
  * Reset daily quests at 3:00 AM server time.
  */
-Schedule::call('app:reset-daily-quests')
+Schedule::command('app:reset-daily-quests')
     ->dailyAt('03:00')
     ->name('reset-daily-quests')
     ->withoutOverlapping();
@@ -21,9 +26,9 @@ Schedule::call('app:reset-daily-quests')
  *
  * This is a resource-intensive job, so we run it after the 3am job to avoid overloading the server.
  */
-Schedule::job(BuildAddonDataFile::class)->dailyAt('03:15');
+Schedule::job(BuildAddonDataFile::class)->dailyAt('03:15')->name('build-addon-data-file')->withoutOverlapping();
 
 /**
  * This should be the last job to run each day.
  */
-Schedule::call('model:prune')->dailyAt('06:00')->name('model-prune');
+Schedule::command('model:prune')->dailyAt('06:00')->name('model-prune');
