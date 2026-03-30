edc32ffcb8b5c4d1e1253da6f2348cc5ed1f8c03
Ben Argo <benargo@users.noreply.github.com>
2026-03-30 18:17:18 +0100

    Add a listener for pre-fetching media from Blizzard for items when they are saved to the database.

---
+++ app/Listeners/PrefetchMediaForItem.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ItemSaved;
+use App\Services\Blizzard\MediaService;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\Middleware\Skip;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
+
+class PrefetchMediaForItem implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct(
+        protected MediaService $mediaService,
+    ) {}
+
+    /**
+     * Get the middleware the listener should pass through.
+     *
+     * @return array<int, object>
+     */
+    public function middleware(ItemSaved $event): array
+    {
+        return [
+            new WithoutOverlapping($event->item->id),
+            Skip::when(function () use ($event): bool {
+                return $event->item->icon === null;
+            }),
+        ];
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(ItemSaved $event): void
+    {
+        $this->mediaService->downloadAssets($event->item->icon->assets);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,7 @@ use App\Listeners\DispatchCharacterUpdates;
 use App\Listeners\FetchGuildRoster;
 use App\Listeners\FlushLootCouncilCache;
 use App\Listeners\HandleGrmUpload;
+use App\Listeners\PrefetchMediaForItem;
 use App\Listeners\ScheduleAddonExportBuild;
 use App\Services\Blizzard\ItemService;
 use App\Services\Blizzard\MediaService;
@@ -20,11 +21,12 @@ abstract class TestCase extends BaseTestCase
 
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        $this->mock(ScheduleAddonExportBuild::class)->shouldReceive('handle');
-        $this->mock(FetchGuildRoster::class)->shouldReceive('handle');
         $this->mock(DispatchCharacterUpdates::class)->shouldReceive('handle');
+        $this->mock(FetchGuildRoster::class)->shouldReceive('handle');
         $this->mock(FlushLootCouncilCache::class)->shouldReceive('handle');
         $this->mock(HandleGrmUpload::class)->shouldReceive('handle');
+        $this->mock(PrefetchMediaForItem::class)->shouldReceive('handle');
+        $this->mock(ScheduleAddonExportBuild::class)->shouldReceive('handle');
 
         $this->mock(ItemService::class, function ($mock) {
             $mock->shouldReceive('find')
