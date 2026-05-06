19d5d6a7a2e01bb700aa402dbae960075db1a162
Ben Argo <benargo@users.noreply.github.com>
2026-05-06 11:49:59 +0100

    refactor: reorder boss entries in BossSeeder for correct encounter order

--- database/seeders/BossSeeder.php
+++ database/seeders/BossSeeder.php
@@ -14,18 +14,18 @@ class BossSeeder extends Seeder
     {
         $bosses = [
             // Karazhan (raid_id: 1)
-            ['id' => 1, 'name' => 'Attumen the Huntsman', 'raid_id' => 1, 'encounter_order' => 1],
-            ['id' => 2, 'name' => 'Servants\' Quarters', 'raid_id' => 1, 'encounter_order' => 11],
-            ['id' => 3, 'name' => 'Moroes', 'raid_id' => 1, 'encounter_order' => 2],
-            ['id' => 4, 'name' => 'Maiden of Virtue', 'raid_id' => 1, 'encounter_order' => 3],
-            ['id' => 5, 'name' => 'Opera Event', 'raid_id' => 1, 'encounter_order' => 4],
-            ['id' => 6, 'name' => 'Nightbane', 'raid_id' => 1, 'encounter_order' => 5],
-            ['id' => 7, 'name' => 'The Curator', 'raid_id' => 1, 'encounter_order' => 6],
-            ['id' => 8, 'name' => 'Terestian Illhoof', 'raid_id' => 1, 'encounter_order' => 7],
-            ['id' => 9, 'name' => 'Shade of Aran', 'raid_id' => 1, 'encounter_order' => 8],
-            ['id' => 10, 'name' => 'Netherspite', 'raid_id' => 1, 'encounter_order' => 9],
-            ['id' => 11, 'name' => 'Chess Event', 'raid_id' => 1, 'encounter_order' => 10],
-            ['id' => 12, 'name' => 'Prince Malchezaar', 'raid_id' => 1, 'encounter_order' => 11],
+            ['id' => 2, 'name' => 'Servants\' Quarters', 'raid_id' => 1, 'encounter_order' => 1],
+            ['id' => 1, 'name' => 'Attumen the Huntsman', 'raid_id' => 1, 'encounter_order' => 2],
+            ['id' => 3, 'name' => 'Moroes', 'raid_id' => 1, 'encounter_order' => 3],
+            ['id' => 4, 'name' => 'Maiden of Virtue', 'raid_id' => 1, 'encounter_order' => 4],
+            ['id' => 5, 'name' => 'Opera Event', 'raid_id' => 1, 'encounter_order' => 5],
+            ['id' => 6, 'name' => 'Nightbane', 'raid_id' => 1, 'encounter_order' => 6],
+            ['id' => 7, 'name' => 'The Curator', 'raid_id' => 1, 'encounter_order' => 7],
+            ['id' => 8, 'name' => 'Terestian Illhoof', 'raid_id' => 1, 'encounter_order' => 8],
+            ['id' => 9, 'name' => 'Shade of Aran', 'raid_id' => 1, 'encounter_order' => 9],
+            ['id' => 10, 'name' => 'Netherspite', 'raid_id' => 1, 'encounter_order' => 10],
+            ['id' => 11, 'name' => 'Chess Event', 'raid_id' => 1, 'encounter_order' => 11],
+            ['id' => 12, 'name' => 'Prince Malchezaar', 'raid_id' => 1, 'encounter_order' => 12],
 
             // Gruul's Lair (raid_id: 2)
             ['id' => 13, 'name' => 'High King Maulgar', 'raid_id' => 2, 'encounter_order' => 1],
