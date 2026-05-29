b876ef8b108a3724f970b9c5c3b874d2e0e62250
Ben Argo <benargo@users.noreply.github.com>
2026-05-29 20:15:58 +0100

    feat: Refactor exception classes to simplify structure and introduce NotFoundException base class

--- app/Http/Integrations/Blizzard/Exceptions/InvalidRaceException.php
+++ app/Http/Integrations/Blizzard/Exceptions/InvalidRaceException.php
@@ -2,56 +2,7 @@
 
 namespace App\Http\Integrations\Blizzard\Exceptions;
 
-use App\Services\Blizzard\Exceptions\BlizzardRequestException;
-use Saloon\Exceptions\Request\Statuses\NotFoundException;
-use Saloon\Http\Response;
-use Throwable;
-
-class InvalidRaceException extends NotFoundException implements BlizzardRequestException
+class InvalidRaceException extends NotFoundException
 {
-    /**
-     * @param  array<string, mixed>|null  $body
-     */
-    public function __construct(
-        public readonly string $method,
-        public readonly string $endpoint,
-        public readonly int $blizzardStatus,
-        Response $response,
-        public readonly ?string $blizzardCode = null,
-        public readonly ?array $body = null,
-        ?Throwable $previous = null,
-    ) {
-        parent::__construct(
-            $response,
-            "Playable race not found: {$this->method} {$this->endpoint} (status {$this->blizzardStatus})",
-            $this->blizzardStatus,
-            $previous,
-        );
-    }
-
-    public function getMethod(): string
-    {
-        return $this->method;
-    }
-
-    public function getEndpoint(): string
-    {
-        return $this->endpoint;
-    }
-
-    public function getBlizzardStatus(): int
-    {
-        return $this->blizzardStatus;
-    }
-
-    public function getBlizzardCode(): ?string
-    {
-        return $this->blizzardCode;
-    }
-
-    /** @return array<string, mixed>|null */
-    public function getBlizzardBody(): ?array
-    {
-        return $this->body;
-    }
+    protected string $prefix = 'Playable race not found:';
 }
