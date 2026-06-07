e6c8b8e3c1a609b85ca74e76027833ff5ceaa47c
Ben Argo <benargo@users.noreply.github.com>
2026-06-07 08:51:43 +0100

    chore: use multiple middleware attributes in ServeIconController for cleanliness

--- app/Http/Controllers/ServeIconController.php
+++ app/Http/Controllers/ServeIconController.php
@@ -11,7 +11,8 @@ use Illuminate\Routing\Attributes\Controllers\Middleware;
 use Illuminate\Support\Str;
 use Throwable;
 
-#[Middleware('signed', 'throttle:icons')]
+#[Middleware('signed')]
+#[Middleware('throttle:icons')]
 class ServeIconController extends Controller implements HasBlizzardIcons
 {
     private const LONG_CACHE = 'public, max-age=31536000, immutable';
