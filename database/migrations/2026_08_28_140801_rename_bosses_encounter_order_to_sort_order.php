ca9ce14a65db62a535bce10aad1d3e8f3968db5c
Ben Argo <benargo@users.noreply.github.com>
2026-08-28 13:36:57 +0100

    feat: add migration and tests to rename encounter_order to sort_order for bosses

---
+++ database/migrations/2026_08_28_140801_rename_bosses_encounter_order_to_sort_order.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bosses', function (Blueprint $table) {
+            $table->renameColumn('encounter_order', 'sort_order');
+        });
+
+        Schema::table('bosses', function (Blueprint $table) {
+            $table->unsignedSmallInteger('sort_order')->nullable(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bosses', function (Blueprint $table) {
+            $table->integer('sort_order')->nullable(false)->change();
+        });
+
+        Schema::table('bosses', function (Blueprint $table) {
+            $table->renameColumn('sort_order', 'encounter_order');
+        });
+    }
+};
