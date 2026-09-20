627d22f372e06ff6fe9e6d6acdfcf5352602dc85
Ben Argo <benargo@users.noreply.github.com>
2026-09-20 17:36:28 +0100

    feat: update ToolNavLink component for enhanced navigation links

--- resources/js/Components/ToolNav.jsx
+++ resources/js/Components/ToolNav.jsx
@@ -1,3 +1,5 @@
+import { Link } from "@inertiajs/react";
+
 export default function ToolNav({ children }) {
     return (
         <nav className="bg-ground-900 shadow">
@@ -5,5 +7,16 @@ export default function ToolNav({ children }) {
                 <div className="flex min-h-12 flex-row flex-wrap items-center justify-between gap-x-2">{children}</div>
             </div>
         </nav>
+    );
+}
+
+export function ToolNavLink({ as: Component = Link, className = "", children, ...props }) {
+    return (
+        <Component
+            className={`my-2 flex flex-row items-center rounded-md border border-transparent p-2 text-sm font-medium text-white hover:border-primary hover:bg-ground-800 active:border-primary ${className}`}
+            {...props}
+        >
+            {children}
+        </Component>
     );
 }
